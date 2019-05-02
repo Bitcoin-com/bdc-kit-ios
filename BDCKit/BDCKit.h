5d4a48fb14173eb6ef7679bbce36c518a27b1bf6
jbdtky <jb@bitcoin.com>
2019-05-02 09:53:22 +0200

    :fire: init repo

---
+++ BDCKit/BDCKit.h
@@ -0,0 +1,19 @@
+//
+//  BDCKit.h
+//  BDCKit
+//
+//  Created by Jean-Baptiste Dominguez on 2019/05/02.
+//  Copyright © 2019 Bitcoin.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BDCKit.
+FOUNDATION_EXPORT double BDCKitVersionNumber;
+
+//! Project version string for BDCKit.
+FOUNDATION_EXPORT const unsigned char BDCKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BDCKit/PublicHeader.h>
+
+
