83df37d0c6ebf3abfb32284db4ec2938c7a6e705
JCurativo <jen@bitcoin.com>
2019-08-16 11:22:05 +0800

    Fix didSelect issues on tableView when there's a keyboard

--- BDCKit/UIComponents/Extensions/UIViewController+Extensions.swift
+++ BDCKit/UIComponents/Extensions/UIViewController+Extensions.swift
@@ -30,6 +30,7 @@ public extension UIViewController {
     
     func hideKeyboardWhenTappedAround() {
         let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
         view.addGestureRecognizer(tap)
     }
     
